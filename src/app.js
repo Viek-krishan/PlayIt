78e26372266a4d43d08dbedf83fb3378007f055c
vivek krishan <vivekkrishan440@gmail.com>
2024-01-25 21:10:18 +0530

    Utils code

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
