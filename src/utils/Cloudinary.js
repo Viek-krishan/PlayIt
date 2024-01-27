db61e29e1ecde0d009241f83c8d8fde1af241524
vivek krishan <vivekkrishan440@gmail.com>
2024-01-27 11:50:31 +0530

    Created Multer and cloudinary

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRATE,
+});
+
+const UploadFileToCloudinary = async (LocalFilePath) => {
+  try {
+    if (!LocalFilePath) return null;
+
+    const Response = await cloudinary.uploader.upload(LocalFilePath, {
+      resource_type: auto,
+    });
+
+    console.log("file uploaded successfully", Response);
+
+    return Response;
+  } catch (error) {
+    fs.unlinkSync(LocalFilePath);
+    return null;
+  }
+};
+
+export { UploadFileToCloudinary };
